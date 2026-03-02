ad6920cb631be9d7e9799e23407808783aab3702
Gabriel Bianchi Britto (Bolsista) <gabriel.britto@cefetes.br>
2026-03-02 13:00:38 -0300

    feat: add contactMapper variable

--- email-notification-system-backend/src/main/java/com/example/demo/services/ContactService.java
+++ email-notification-system-backend/src/main/java/com/example/demo/services/ContactService.java
@@ -1,26 +1,64 @@
 package com.example.demo.services;
 
 import com.example.demo.entity.Contact;
+import com.example.demo.mapper.ContactMapper;
 import com.example.demo.repository.ContactRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ContactService {
+public class ContactService
+{
     private final ContactRepository contactRepository;
+    private final ContactMapper mapper;
 
-    public void save()
+    @Transactional
+    public Contact create(Contact contact)
     {
-        Contact contact = new Contact();
-        contact.setName("Cauã Andrade");
-        contact.setEmail("caua@gmail.com");
-        contact.setTelephone("28999745874");
         contact.setCreatedAt(Instant.now());
         contact.setUpdatedAt(Instant.now());
-
-        contactRepository.save(contact);
+        return contactRepository.save(contact);
     }
+
+    public List<Contact> findAll()
+    {
+        return contactRepository.findAll();
+    }
+
+    public List<Contact> findAllById(List<Long> idList) {
+        return contactRepository.findAllById(idList);
+    }
+
+    public Contact findById(Long id) {
+        return contactRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("contact entity not found"));
+    }
+
+    @Transactional
+    public Contact update(Long id, Contact updatedContact)
+    {
+        Contact contact = findById(id);
+
+        contact.setName(updatedContact.getName());
+        contact.setEmail(updatedContact.getEmail());
+        contact.setTelephone(updatedContact.getTelephone());
+        contact.setUpdatedAt(Instant.now());
+
+        return contactRepository.save(contact);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+
+    }
+
+
 }
