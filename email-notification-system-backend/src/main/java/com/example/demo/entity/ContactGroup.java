fa4720ce72470f00994a8ce93647575d601ad834
Gabriel Bianchi Britto (Bolsista) <gabriel.britto@cefetes.br>
2026-03-03 10:44:29 -0300

    feat(contact_group): create relationship class between contact and group

---
+++ email-notification-system-backend/src/main/java/com/example/demo/entity/ContactGroup.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import com.example.demo.entity.mapsId.ContactGroupId;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "contact_has_group")
+@Getter
+@Setter
+public class ContactGroup {
+
+    @EmbeddedId
+    private ContactGroupId contactGroupId;
+
+    @ManyToOne
+    @MapsId("groupId")
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @ManyToOne
+    @MapsId("contactId")
+    @JoinColumn(name = "contact_id")
+    private Contact contact;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+}
