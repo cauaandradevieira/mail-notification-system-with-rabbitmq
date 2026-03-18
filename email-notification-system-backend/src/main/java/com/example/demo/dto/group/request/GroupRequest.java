af2caf5720014f5a5c1f8727da924c22ff6aff8f
cauaandradevieiraa <cauaandradevieiraa@gmail.com>
2026-03-18 09:16:32 -0300

    feat(group-request): add color attribute

--- email-notification-system-backend/src/main/java/com/example/demo/dto/group/request/GroupRequest.java
+++ email-notification-system-backend/src/main/java/com/example/demo/dto/group/request/GroupRequest.java
@@ -3,6 +3,7 @@ package com.example.demo.dto.group.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record GroupRequest(
-        @NotBlank String name
+        @NotBlank String name,
+        String color
 ) {
 }
