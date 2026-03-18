9fcc0b5d1d2e5436f3b6355441cbfecd84e5ec93
cauaandradevieiraa <cauaandradevieiraa@gmail.com>
2026-03-18 09:12:11 -0300

    feat(email-service): add email message formatting

--- email-notification-system-backend/src/main/java/com/example/demo/services/EmailService.java
+++ email-notification-system-backend/src/main/java/com/example/demo/services/EmailService.java
@@ -1,6 +1,8 @@
 package com.example.demo.services;
 
 import com.example.demo.dto.email.request.EmailRequest;
+import com.example.demo.entity.Contact;
+import com.example.demo.rabbit.message.EmailMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,13 +13,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender javaMailSender;
+    private final ContactService contactService;
 
-    public void send(EmailRequest request)
+    public void send(EmailMessage request)
     {
+        // pegar o contato no banco e verificar se existe
+        Contact contact = contactService.findById(request.contactId());
+
+        // request contem caracteres como {nome} e etc... que deve ser trocados por dados
+
+        // mensagem = Olá {nome}, hoje dia 5 estaremos fazendo uma festa em comemoração do dia do funcionario
+
+        String messageFormatted = request.body()
+                .replace("{nome}", contact.getName())
+                .replace("{email}", contact.getEmail());
+
+        // se a mensagem conter o caracter {nome} faça isso:
+
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setTo(request.to());
+        simpleMailMessage.setTo(contact.getEmail());
         simpleMailMessage.setSubject(request.subject());
-        simpleMailMessage.setText(request.body());
+        simpleMailMessage.setText(messageFormatted);
 
         javaMailSender.send(simpleMailMessage);
     }
