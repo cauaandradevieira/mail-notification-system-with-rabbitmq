64cb35b44b12a1c463ae0ca499b9539c304f34dd
cauaandradevieiraa <cauaandradevieiraa@gmail.com>
2026-03-18 08:19:52 -0300

    feat(mapper): add groupWithContactsQuantityDTO method

--- email-notification-system-backend/src/main/java/com/example/demo/mapper/ContactGroupMapper.java
+++ email-notification-system-backend/src/main/java/com/example/demo/mapper/ContactGroupMapper.java
@@ -2,17 +2,18 @@ package com.example.demo.mapper;
 
 import com.example.demo.dto.contact.request.ContactRequest;
 import com.example.demo.dto.contact.response.ContactResponse;
-import com.example.demo.dto.contact_group.response.ContactGroupResponse;
-import com.example.demo.dto.contact_group.response.ContactsWithGroupResponse;
+import com.example.demo.dto.contact_group.projections.GroupsWithContactsQuantityProjection;
+import com.example.demo.dto.contact_group.response.GroupWithContactsResponse;
+import com.example.demo.dto.contact_group.response.GroupsWithContactsQuantityDTO;
 import com.example.demo.entity.Contact;
 import com.example.demo.entity.ContactGroup;
 import com.example.demo.entity.Group;
-import jakarta.validation.ConstraintValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -21,7 +22,7 @@ public class ContactGroupMapper {
     private final ContactMapper contactMapper;
     private final GroupMapper groupMapper;
 
-    public ContactsWithGroupResponse toContactsWithGroupDto(List<ContactGroup> contactGroupList)
+    public GroupWithContactsResponse toGroupWithContactsDTO(List<ContactGroup> contactGroupList)
     {
         List<Contact> contactList = contactGroupList.stream()
                 .map(ContactGroup::getContact)
@@ -29,38 +30,21 @@ public class ContactGroupMapper {
 
         Group group = contactGroupList.getFirst().getGroup();
 
-        return ContactsWithGroupResponse.builder()
+        return GroupWithContactsResponse.builder()
                 .group(groupMapper.toDto(group))
                 .contacts(contactMapper.toDtoList(contactList))
                 .build();
     }
 
-    public ContactResponse toDto(Contact contact)
+    public List<GroupsWithContactsQuantityDTO> toGroupsWithContactsQuantityDTO(List<GroupsWithContactsQuantityProjection> projection)
     {
-        return ContactResponse.builder()
-                .id(contact.getId())
-                .email(contact.getEmail())
-                .telephone(contact.getTelephone())
-                .createdAt(contact.getCreatedAt())
-                .updatedAt(contact.getUpdatedAt())
-                .build();
-    }
-
-    public List<ContactResponse> toDtoList(List<Contact> contactList)
-    {
-        return contactList.stream()
-                .map(this::toDto)
+        return projection.stream()
+                .map(p -> GroupsWithContactsQuantityDTO.builder()
+                        .groupResponse(groupMapper.toDto(p.getGroup()))
+                        .contactsQuantity(p.getContactQuantity())
+                        .build())
                 .toList();
     }
 
-    public Contact toEntity(ContactRequest request)
-    {
-        Contact contact = new Contact();
 
-        contact.setName(request.name());
-        contact.setEmail(request.email());
-        contact.setTelephone(request.telephone());
-
-        return contact;
-    }
 }
